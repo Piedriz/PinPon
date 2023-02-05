8dcfc6a4361a620153b8a3e0c6fb829fc4a41324
Santiago Piedriz <piedrizpasos@gmail.com>
2023-02-04 23:33:46 -0500

    adding routes to nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,6 +5,7 @@ import { TrendingIcon } from '../Icons/TrendingIcon'
 import { UploadVideoIcon } from '../Icons/UploadVideoIcon'
 import { NotificationIcon } from '../Icons/NotificationIcon'
 import { PerfilIcon } from '../Icons/PerfilIcon'
+import { Link } from 'wouter'
 
 // ...
 
@@ -13,10 +14,10 @@ export const NavBar = () => {
 
     <nav className='navBar'>
 
-      <div><HomeIcon /></div>
+      <Link href='/' className='active'><div><HomeIcon /></div></Link>
       <div><TrendingIcon /></div>
       {/* <div><RiVideoUploadFill size={50} color='green' /></div> */}
-      <div><UploadVideoIcon /></div>
+      <Link href='/upload' className='active'><div><UploadVideoIcon /></div></Link>
       <div><NotificationIcon /></div>
       <div><PerfilIcon /></div>
 
